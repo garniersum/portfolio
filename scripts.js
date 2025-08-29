31edf18182daefe04e8c9d57ede435bf02615647
Carlos Gomez <carlos.gomez.mobile@gmail.com>
2025-08-29 02:21:20 -0600

    menu bar working better and smooth effect

--- scripts.js
+++ scripts.js
@@ -1,3 +1,12 @@
 function toggleMobileMenu(){
   document.getElementById("menu").classList.toggle("active");
 }
+
+function waitAndCloseMenu(){
+  setTimeout(() => {
+      document.getElementById("menu").classList.remove("active");
+    }, 1000);
+}
+function closeMenu(){
+      document.getElementById("menu").classList.remove("active");
+}
