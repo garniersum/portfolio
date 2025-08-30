3696ef909c121e7863d18c79028d82b7dd0d70d6
Carlos Gomez <carlos.gomez.mobile@gmail.com>
2025-08-29 20:02:17 -0600

    Typing effect added, hero img in smaller size

--- scripts.js
+++ scripts.js
@@ -10,3 +10,12 @@ function waitAndCloseMenu(){
 function closeMenu(){
       document.getElementById("menu").classList.remove("active");
 }
+
+  document.addEventListener("DOMContentLoaded", function() {
+    new Typed("#typing", {
+      strings: ["Carlos Gómez", "Frontend Developer", "Web Designer"],
+      typeSpeed: 100,
+      BackSpeed: 60,
+      loop: true,
+    });
+  });
